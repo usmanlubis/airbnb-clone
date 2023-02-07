6d8f08671fedac6fafb925e2eadb70e8a14ec3bc
usmanlubis <usmanlubis11@gmail.com>
2023-02-07 20:49:37 +0700

    install tailwindcss and customize file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
