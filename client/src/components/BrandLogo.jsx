a833755bef75a2549c36b8c293333f0de37c5d3c
usmanlubis <usmanlubis11@gmail.com>
2023-02-08 11:00:59 +0700

    create BrandLogo component

---
+++ client/src/components/BrandLogo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function BrandLogo() {
+  return (
+    <div className="flex gap-1 items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-8 h-8 text-bnb-pink -rotate-90"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+        />
+      </svg>
+      <p className="text-2xl text-bnb-pink font-bold">airbnb</p>
+    </div>
+  );
+}
+
+export default BrandLogo;
