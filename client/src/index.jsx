7ab3e168f5b15cd2789f27ed09201dc974f789ae
usmanlubis <usmanlubis11@gmail.com>
2023-02-14 11:15:17 +0700

    migrate to Vite

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,5 +13,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>,
 );
-
-reportWebVitals();
