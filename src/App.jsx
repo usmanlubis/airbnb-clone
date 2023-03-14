d07bdb8209b4441ae1e29b57c8a8bef6fb8cbdb5
usmanlubis <usmanlubis11@gmail.com>
2023-03-14 09:29:21 +0700

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/airbnb-clone/" element={<HomePage />} />
+      <Route path="/airbnb-clone/login" element={<Login />} />
     </Routes>
   );
 }
