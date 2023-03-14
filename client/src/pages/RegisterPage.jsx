051861ad98d04ad848dbd54cf83f2711652d0e48
usmanlubis <usmanlubis11@gmail.com>
2023-03-14 13:11:12 +0700

    make register

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 function RegisterPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function regist(event) {
+    event.preventDefault();
+    axios.get('https://localhost:9000/test');
+  }
   return (
     <div className="grow mt-4">
       <h2 className="text-2xl text-center mb-8">Register</h2>
-      <form className="flex flex-col max-w-md mx-auto gap-6">
-        <input type="text" placeholder="Your Name" />
-        <input type="email" placeholder="yourmail@mail.com" />
-        <input type="password" placeholder="Your password" />
-        <button type="submit" className="primary">Login</button>
+      <form className="flex flex-col max-w-md mx-auto gap-6" onSubmit={regist}>
+        <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
+        <input type="email" placeholder="yourmail@mail.com" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" placeholder="Your password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit" className="primary">Sign Up</button>
       </form>
       <div className="text-center text-sm mt-2 text-slate-500">
         Already have an account?
