6dde87f6e9adb6ca1f15e04cc3b01f78de63b101
usmanlubis <usmanlubis11@gmail.com>
2023-03-14 09:29:35 +0700

    create link with Link component

--- src/components/header/header/NavBar.jsx
+++ src/components/header/header/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
@@ -17,8 +18,8 @@ function NavBar() {
           <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
         </svg>
         <div className="w-[225px] border-2 shadow-xl py-2 rounded-xl text-slate-600 absolute top-12 right-0">
-          <a className="px-4 py-2 block hover:bg-black/10 font-[500]" href="/airbnb-clone/register">Sign up</a>
-          <a className="px-4 py-2 block hover:bg-black/10" href="/airbnb-clone/login">Log in</a>
+          <Link className="px-4 py-2 block hover:bg-black/10 font-[500]" to="/airbnb-clone/register">Sign up</Link>
+          <Link className="px-4 py-2 block hover:bg-black/10" to="/airbnb-clone/login">Log in</Link>
         </div>
       </div>
     </nav>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      Login Page
+    </div>
+  );
+}
+
+export default Login;
+
