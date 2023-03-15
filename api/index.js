b807bec7fdf98765dcd749dd8f54b08425858f69
usmanlubis <usmanlubis11@gmail.com>
2023-03-15 10:46:42 +0700

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User')
+require('dotenv').config()
 const app = express();
 
 app.use(express.json())
 app.use(cors());
 
+mongoose.connect(process.env.MONGO_URL)
 app.post("/register", (req, res) => {
   const {name, email, password} = req.body;
-  res.json({name, email, password})
+  User.create({
+    name,
+    email,
+    password,
+  })
 })
 app.listen(9000);
