9afb97d100594a47b8b2475de28bca25a84a0064
usmanlubis <usmanlubis11@gmail.com>
2023-03-15 11:05:32 +0700

    encrypt passwod using bcrypt

--- api/index.js
+++ api/index.js
@@ -2,19 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User')
+const bcrypt = require('bcryptjs');
 require('dotenv').config()
 const app = express();
 
 app.use(express.json())
 app.use(cors());
 
+const bcryptSalt = bcrypt.genSaltSync(16)
+
 mongoose.connect(process.env.MONGO_URL)
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const {name, email, password} = req.body;
-  User.create({
+  const userDetail = await User.create({
     name,
     email,
-    password,
-  })
-})
+    password:bcrypt.hashSync(password, bcryptSalt),
+  });
+
+  res.json(userDetail)
+});
 app.listen(9000);
