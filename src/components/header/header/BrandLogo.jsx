925457c8de4424603c290799ff7db2a18c8fb597
usmanlubis <usmanlubis11@gmail.com>
2023-03-16 13:15:55 +0700

    change brand logo link

--- src/components/header/header/BrandLogo.jsx
+++ src/components/header/header/BrandLogo.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function BrandLogo() {
   return (
-    <Link href="/airbnb-clone" className="hidden md:flex gap-1 items-center">
+    <Link href="/airbnb-clone/" className="hidden md:flex gap-1 items-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
